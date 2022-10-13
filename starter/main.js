039c0265332767e90aad337c6566a99dc68c0831
Helenekhay <helenemieee@gmail.com>
2022-10-13 05:44:55 -0700

    toggle hamburger

---
+++ starter/main.js
@@ -0,0 +1,20 @@
+
+
+
+// ESS
+
+// console.log("send me your aza")
+// alert(writeup)
+// var writeup = "i don dey code since"
+// console.log(hamburgerdiv)
+// console.log (document)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+
+
+
+function talkanddo() {
+    hamburgerdiv.classList.toggle("shopburger")
+    
+}
