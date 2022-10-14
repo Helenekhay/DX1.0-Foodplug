cf2696d4d12e0c3f7ccb498291cf5f6add0c4315
Helenekhay <helenemieee@gmail.com>
2022-10-14 04:45:16 -0700

    completed header section for small/large screen

--- starter/main.js
+++ starter/main.js
@@ -9,11 +9,12 @@
 // console.log(hamburgerdiv)
 // console.log (document)
 
-var hamburgerdiv = document.querySelector(".hamburger")
+var hamburgerdiv = document.querySelector(".hamburger");
 var mobilelinks = document.querySelector(".mobile-linkholder");
-
+var backdrop = document.querySelector(".backdrop");
 
 function talkanddo() {
     hamburgerdiv.classList.toggle("shopburger")
-    mobilelinks.classList.toggle("show-mobile-linkholder")    
+    mobilelinks.classList.toggle("show-mobile-linkholder")
+    backdrop.classList.toggle("show-backdrop")    
 }
